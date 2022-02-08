f3763bd45379593fb928bb08dd63f2bd24dd011f
JustinMitchell <mitchelljm04@gmail.com>
2022-02-07 21:55:28 -0500

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  let newTask = document.getElementById('new-task-description');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(newTask.value);
+    form.reset();
+  })
+  
 });
+
+function buildToDo (toDo){
+  let newPara = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleRemove)
+  btn.textContent = 'X'
+  newPara.textContent = `${toDo} `;
+  console.log(newPara)
+  document.querySelector('#list').appendChild(newPara);
+  newPara.appendChild(btn)
+}
+
+function handleRemove(e){
+  e.target.parentNode.remove();
+  if ('clicked' === true){
+    alert('All Done.')
+  }
+}
+
+
